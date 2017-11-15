2aac5532c7f56d2d8d017fb02160729b6fd6f9d1
ocboogie <boogie@mikulec.com>
2017-11-14 22:32:54 -0800

    Add an example for error handling

---
+++ examples/errorHandling/app/index.js
@@ -0,0 +1,52 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import Cheerio from "cheerio";
+
+import CheerioReactBind, { update } from "../../../";
+
+// Custom tags of type { [key: string]: react.component; } in typescript
+const tags = {
+  container: ({ children }) => <div>{children}</div>,
+  smalltext: ({ children }) => <p>{children}</p>,
+  bigtext: ({ children }) => <h1>{children}</h1>
+};
+
+// Load the Cheerio dom
+const $ = Cheerio.load(
+  `
+  <container id="root"></container>
+  `,
+  { xmlMode: true }
+);
+
+// Called a error occurs
+function errorHandler(msg) {
+  // eslint-disable-next-line no-alert
+  alert(`There was a error: ${msg}`);
+}
+
+ReactDOM.render(
+  <CheerioReactBind
+    errorHandler={errorHandler}
+    $={$}
+    $elem={$("#root").first()}
+    tags={tags}
+  />,
+  document.getElementById("container")
+);
+
+$("#root").append("<smalltext>Very small text</smalltext>");
+$("#root").append("<bigtext>Very big text</bigtext>");
+// Nothing changed on the view yet
+setTimeout(() => {
+  // Now the view changed
+  update($("#root"));
+}, 1000);
+
+setTimeout(() => {
+  $("#root").append("<div>An error will be thrown</div>");
+  update($("#root"));
+  // This will throw an error because div is not a tag
+  // Note: normal HTML tags are not allowed
+  // This is why div is an unknown tag
+}, 2000);
