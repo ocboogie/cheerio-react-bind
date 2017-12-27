e16d83339b4c8a8df849f20e1c549cb54bcc4e1f
Eamon mikulec <boogie@mikulec.com>
2017-12-26 18:05:52 -0800

    Update tests Add enzyme Update jest to 22.0.4

--- tests/CheerioReactBind.js
+++ tests/CheerioReactBind.js
@@ -1,7 +1,7 @@
 /* eslint react/prop-types: "off" */
 import React from "react";
 import Cheerio from "cheerio";
-import renderer from "react-test-renderer";
+import * as Enzyme from "enzyme";
 
 import CheerioReactBind, { update } from "../src/index";
 
@@ -13,7 +13,7 @@ const tags = {
 
 const $ = Cheerio.load(
   `
-  <div>
+  <div foo=bar>
     <p>Paragraph</p>
   </div>
 `,
@@ -21,45 +21,44 @@ const $ = Cheerio.load(
 );
 
 test("modifying the Cheerio dom updates the React dom with custom tags", () => {
-  const component = renderer.create(
+  const wrapper = Enzyme.mount(
     <CheerioReactBind tags={tags} $elem={$("div").first()} $={$} />
   );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(wrapper.html()).toMatchSnapshot();
 
   $("div").append(`<div>test</div>`);
   update($("div"));
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(wrapper.html()).toMatchSnapshot();
 
   $("p").text("Paragraph test");
   update($("p"));
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(wrapper.html()).toMatchSnapshot();
 });
 
-test("throwns when there is a unknown tag", () => {
-  renderer.create(
-    <CheerioReactBind tags={tags} $elem={$("div").first()} $={$} />
-  );
-  $("div").append("<notag />");
-  expect(() => {
+describe("error handling", () => {
+  test("throwns when there is a unknown tag", () => {
+    Enzyme.mount(
+      <CheerioReactBind tags={tags} $elem={$("div").first()} $={$} />
+    );
+    $("div").append("<notag />");
+    expect(() => {
+      update($("div"));
+    }).toThrow(new TypeError('Unknown tag "notag".'));
+  });
+
+  test("errorHandling runs when an error is thrown", () => {
+    const mockErrorHandling = jest.fn();
+    Enzyme.render(
+      <CheerioReactBind
+        errorHandler={mockErrorHandling}
+        tags={tags}
+        $elem={$("div").first()}
+        $={$}
+      />
+    );
+    $("div").append("<notag />");
     update($("div"));
-  }).toThrow(new TypeError('Unknown tag "notag".'));
-});
 
-test("errorHandling runs when an error is thrown", () => {
-  const mockErrorHandling = jest.fn();
-  renderer.create(
-    <CheerioReactBind
-      errorHandler={mockErrorHandling}
-      tags={tags}
-      $elem={$("div").first()}
-      $={$}
-    />
-  );
-  $("div").append("<notag />");
-  update($("div"));
-
-  expect(mockErrorHandling.mock.calls[0][0]).toBe('Unknown tag "notag".');
+    expect(mockErrorHandling.mock.calls[0][0]).toBe('Unknown tag "notag".');
+  });
 });
